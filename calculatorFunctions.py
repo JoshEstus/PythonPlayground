39a21b78199b689ea66455d6f529d54b24dadc09
Josh Estus <josh.estus@gmail.com>
2015-01-24 19:41:27 -0500

    Initial Commit

---
+++ calculatorFunctions.py
@@ -0,0 +1,12 @@
+#This will print the menu
+#Takes no Parameters
+def printMenu():
+	print("1: Add")
+	print("2: Subtract")
+	print("3: Multiply")
+	print("4: Divide")
+	print("5: Exit")
+
+#This will ask for the user input and save it as a variable
+def userInput():
+	input("Your Choice: ")
